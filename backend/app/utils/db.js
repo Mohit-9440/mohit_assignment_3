c8df357d723ca7dd34627405ba0ea6eade2be440
Mohit <mohitsharma8864@gmail.com>
2023-07-05 01:36:48 +0530

    .env changes

--- backend/app/utils/db.js
+++ backend/app/utils/db.js
@@ -1,10 +1,11 @@
 // app/utils/db.js
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mohitsharma8864:mohitsharma@cluster0.fbv1o1i.mongodb.net/?retryWrites=true&w=majority/mydatabase', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
