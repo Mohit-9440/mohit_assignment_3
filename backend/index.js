d7a2eb2dde278b98955547b42827bb1def99de87
Mohit <mohitsharma8864@gmail.com>
2023-07-05 01:55:18 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,17 @@ const connectToDatabase = require('./app/utils/db');
 const userRoutes = require('./app/routes/userRoutes');
 
 const app = express();
+
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://mohit-assignment-3.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials:true
+  }
+));
 
 // Connect to MongoDB
 connectToDatabase();
